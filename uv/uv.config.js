997e4da26e50f5d276e9b735684d3bc1ad7b29a9
charliegotkirked-star <charliegotkirked@myyahoo.com>
2026-06-18 21:56:59 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://data-stream.charliegotkirked.workers.dev/',
+    bare: 'wss://data-stream.charliegotkirked.workers.dev/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
